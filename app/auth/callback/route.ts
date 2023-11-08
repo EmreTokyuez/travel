c90a6426e183158f07ff1b9da3c875d0f8c54c43
githubt@tokyuez.net <github@tokyuez.net>
2023-11-08 09:09:53 +0100

    edge

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -20,3 +20,4 @@ export async function GET(request: NextRequest) {
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin);
 }
+export const runtime = "edge";
