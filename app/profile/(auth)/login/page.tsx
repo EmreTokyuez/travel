324ee885b693c6160d8fe3e3bc3956c7973ce37d
githubt@tokyuez.net <github@tokyuez.net>
2023-11-08 08:53:50 +0100

    login

--- app/profile/(auth)/login/page.tsx
+++ app/profile/(auth)/login/page.tsx
@@ -3,16 +3,17 @@ import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 require("dotenv").config();
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+
 const Login = () => {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
   return (
     <Auth
       supabaseClient={supabase}
       appearance={{ theme: ThemeSupa }}
-      providers={["google", "facebook", "twitter"]}
+      providers={["google"]}
     />
   );
 };
