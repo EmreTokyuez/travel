cf6500d177deab8e4e28b1eddcc9cf51aabdb8bc
githubt@tokyuez.net <github@tokyuez.net>
2023-11-08 08:39:30 +0100

    change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { createClient } from "@supabase/supabase-js";
 require("dotenv").config();
-
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -29,14 +28,11 @@ export default function Home() {
             </p>
             <button className="btn btn-primary">Get travelling!</button>
           </div>
-
           <Auth
             supabaseClient={supabase}
             appearance={{ theme: ThemeSupa }}
             providers={["google"]}
-
           />{" "}
-
         </div>
       </div>{" "}
     </div>
